3a37365a36e901cf32147912b63bbf5f523e1a64
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2023-01-03 19:20:24 -0800

    FFmpeg BT.709
    YES! YES, YES, YES, YES, YES!!! This has been haunting me the past few days, since I was last working on making art tracks for my music. When creating a video from a PNG image, the colors haven't looked the best from the conversion, and I have been scouring the web to find out how to fix it. Setting the scale out color matrix property in the `-vf` flag totally fixes it! It still doesn't look color-for-color to the original PNG, but that's expected. Now the output video looks identical in color to a simiarly-exported iMovie project from the same files, but it's now also 10x smaller, thanks to extra optimizations you can tell FFmpeg to make. So, now a video file being around 77 MB is now only 7.7 MB, and it looks the exact same! This isn't a new thing since I've started looking into FFmpeg, but it's different now, because both videos have the same quality now, and you get the smaller size benefits.
    
    I need to make a StackOverflow account just to thank whoever commented this, thank you so much!!! You saved me so many headaches.
    
    ```
    You need to convert to BT.709 matrix using -vf scale=out_color_matrix=bt709 not just tag output using -colorspace bt709.
    ```
    
    https://stackoverflow.com/questions/67073964/how-to-avoid-color-shift-using-ffmpeg-libx264
    
    Oh yeah, found out how to do multiple lines in a shell script too, so I added those in to help myself with understanding all of the different parts of the command.

--- test/ffmpeg.sh
+++ test/ffmpeg.sh
@@ -1,1 +1,15 @@
-ffmpeg -loop 1 -framerate 1 -i 26.png -i 26.m4a -map 0 -map 1:a -c:v libx264 -preset ultrafast -tune stillimage -vf fps=10,format=yuv420p -c:a copy -shortest 26.mp4
+ffmpeg \
+  -loop 1 \
+  -framerate 1 \
+  -i "26.png" \
+  -i "26.m4a" \
+  -map 0 \
+  -map 1:a \
+  -c:v libx264 \
+  -preset ultrafast \
+  -tune stillimage \
+  -vf scale=out_color_matrix=bt709,fps=10 \
+  -pix_fmt yuv420p \
+  -c:a copy \
+  -shortest \
+  "26.mp4"
