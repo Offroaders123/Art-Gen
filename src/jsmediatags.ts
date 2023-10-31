0d2a3602f603842f61403eba4632f8156819e44c
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2023-10-30 18:30:58 -0700

    MediaTags Strictness

--- src/jsmediatags.ts
+++ src/jsmediatags.ts
@@ -15,7 +15,7 @@ export async function readTags(song: Uint8Array): Promise<MediaTags> {
   const allTags: AllTags = await new Promise<AllTags>((onSuccess,onError) => {
     read(song,{
       onSuccess({ tags }) {
-        const artwork = (tags.picture !== undefined) ? fromPictureToDataURL(tags.picture) : undefined;
+        const artwork: string | undefined = tags.picture !== undefined ? fromPictureToDataURL(tags.picture) : undefined;
         const mediaTags: AllTags = Object.assign(tags,{ artwork });
         onSuccess(mediaTags)
       },
@@ -27,22 +27,14 @@ export async function readTags(song: Uint8Array): Promise<MediaTags> {
 
 function fromPictureToDataURL(picture: PictureType): string {
   const { format: type, data } = picture;
-  const base64 = Buffer.from(data).toString("base64");
+  const base64: string = Buffer.from(data).toString("base64");
   return `data:${type};base64,${base64}`;
 }
 
 function toMediaTags({ title, artist, album, artwork }: AllTags): MediaTags {
-  if (title === undefined){
-    throw new Error("Could not load title from song");
-  }
-  if (artist === undefined){
-    throw new Error("Could not load artist from song");
-  }
-  if (album === undefined){
-    throw new Error("Could not load album from song");
-  }
-  if (artwork === undefined){
-    throw new Error("Could not load artwork from song");
-  }
+  if (title === undefined) throw new Error("Could not load title from song");
+  if (artist === undefined) throw new Error("Could not load artist from song");
+  if (album === undefined) throw new Error("Could not load album from song");
+  if (artwork === undefined) throw new Error("Could not load artwork from song");
   return { title, artist, album, artwork };
 }
